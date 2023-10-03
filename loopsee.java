381bdd4fa3a67c1d48cfafcaca72829fdcc8c6fd
MRS0707 <90347900+MRS0707@users.noreply.github.com>
2023-10-03 20:25:39 +0530

    Upload

--- loopsee.java
+++ loopsee.java
@@ -1,8 +1,12 @@
-package Javapractice;
+import java.util.*;
 
 public class loopsee {
     public static void main(String[] args) {
-        System.out.println("Hello Raj")
+        System.out.println("Hello Raj");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Num = ");
+        int num = sc.nextInt();
+        System.out.println("This is Your Num  := " + num);
     }
 
 }
